34743b00f6afda0531058ce9be3f3871d634e640
Coby Benveniste <coby.b@spot.im>
2018-08-05 19:10:45 +0300

    Version 2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GithubRepoDownloader.MixProject do
   def project do
     [
       app: :github_repo_downloader,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
